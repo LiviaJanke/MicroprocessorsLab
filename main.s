96571389cc8a7e0ac8bc8e802e0697ec319fc77d
lme19 <lme19@skbl-418-003.ph.ic.ac.uk>
2023-11-03 11:49:04 +0000

    makin loops

--- main.s
+++ main.s
@@ -9,16 +9,29 @@ main:
 	org	0x100		    ; Main code starts here at address 0x100
 start:
 	movlw 	0x0
-	movwf	TRISB, A	    ; Port C all outputs
+	movwf	TRISC, A	    ; Port C all outputs
 	bra 	test
 loop:
-	movff 	0x06, PORTB
+	movff 	0xFF, PORTC
 	incf 	0x06, W, A
+
+
 test:
 	movwf	0x06, A	    ; Test for end of loop condition
-	movlw 	0x63
+	;movf    PORTD, W, A
+	;movf	PORTD, W, B
+	;movlb	0xf
+	;movf	PORTD, W, B
+	;movf	PORTD, W
+	movlw	0xFF
 	cpfsgt 	0x06, A
+	call	delay
 	bra 	loop		    ; Not yet finished goto start of loop again
 	goto 	0x0		    ; Re-run program from start
+
+
+delay:	decfsz	0x20, A
+	bra	delay
+	return
 
 	end	main
