c69607e828925636096ca409b595b1737a3a41ec
Eichmeyer <lme19@ic.ac.uk>
2023-11-07 12:06:19 +0000

    trying to make triangle wave

--- main.s
+++ main.s
@@ -9,16 +9,17 @@ main:
 	org	0x100		    ; Main code starts here at address 0x100
 start:
 	movlw 	0x0
-	movwf	TRISC, A	    ; Port C all outputs
+	movwf	TRISD, A	    ; Port C all outputs
 	bra 	test
 loop:
-	movff 	0x06, PORTC
+	movff 	0x06, PORTD
 	incf 	0x06, W, A
 test:
 	movwf	0x06, A	    ; Test for end of loop condition
 	movlw 	0xFF
 	cpfsgt 	0x06, A
 	call	hugedelay
+;	call	bigbigbigdelay
 	bra 	loop		    ; Not yet finished goto start of loop again
 	goto 	0x0		    ; Re-run program from start
 
