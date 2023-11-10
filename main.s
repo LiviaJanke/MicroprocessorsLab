a9b3fee0967178b9e724d314955556ee9fdbf7a4
lme19 <lme19@skbl-418-003.ph.ic.ac.uk>
2023-11-10 11:34:29 +0000

    makin loops

--- main.s
+++ main.s
@@ -1,38 +1,26 @@
 	#include <xc.inc>
 
 psect	code, abs
-	
-main:
-	org	0x0
-;	goto	start
 
-	org	0x100		    ; Main code starts here at address 0x100
-start:
-	movlw 	0x0
-	movwf	TRISD, A	; Port C all outputs
-	
-;	CLRF	PORTJ	; Initialize PORTJ by
-			; clearing output latches    
-;	CLRF	LATJ	; Alternate method
-			; to clear output latches
-;	MOVLW	0CFh
-	movlw	0x0
-	movwf	TRISJ, A
-;	bcf	PORTJ, 1 ,0
-	bra 	test
-loop:
-	movff 	0x06, PORTD
-	incf 	0x06, W, A
-test:
-	movwf	0x06, A	    ; Test for end of loop condition
-	movlw 	0xFF
-	cpfsgt 	0x06, A
-	call	delay
-	bra 	loop		    ; Not yet finished goto start of loop again
-;	bsf	PORTJ, 1, 0
-	movff 	0x06, PORTJ
-	goto 	0x0		    ; Re-run program from start
+SPI_MasterInit:	;set clock edge to negative
+    bcf	CKE2	; CKE bit in SSP2STAT
+    ; MSSP enable; CKP=1p; SPI master, clock = Fosc/64 (1MhZ)
+    movlw   (SSP2CON1_SSPEN_MASK)|(SSP2CON1_CKP_MASK)|(SSP2CON1_SSPM1_MASK)
+    movwf   SSP2CON1, A
+    ; SDO2 output; SCK2 output
+    bcf	TRISD, PORTD_SDO2_POSN, A  ; SDO2 output
+    bcf	TRISD, PORTD_SCK2_POSN, A   ; SCK2 ouput
 
+SPI_MasterTransmit: ; start transmission of data (held in W)
+    movwf   SSP2BUF, A	;write data to output buffer
+    
+Wait_Transmit:	    ; Wait for transmission to complete
+    btfss   PIR2, 5  ; Check interrupt flag to see if data has been sent
+    bra	    Wait_Transmit
+    bcf	    PIR2, 5  ; clear interrupt flag
+    call    hugedelay
+    return
+    
 delay:
 	decfsz	0xFF
 	bra delay
@@ -76,10 +64,4 @@ hugedelay:
 	return
 	
 	
-;bigdelay:
-	;movlw	0x00
-;dloop:	decf	0x11, f, A
-	;subwfb	0x10, f, A
-	;return
-	
-	end	main
+
