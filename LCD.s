8a5aa555205ec258d024d2ace144e52f5c30feda
lme19 <lme19@skbl-418-003.ph.ic.ac.uk>
2023-11-21 12:11:26 +0000

    keypad func

--- LCD.s
+++ LCD.s
@@ -1,6 +1,6 @@
 #include <xc.inc>
 
-global  LCD_Setup, LCD_Write_Message
+global  LCD_Setup, LCD_Write_Message, LCD_clear
 
 psect	udata_acs   ; named variables in access ram
 LCD_cnt_l:	ds 1   ; reserve 1 byte for variable LCD_cnt_l
@@ -132,6 +132,15 @@ lcdlp1:	decf 	LCD_cnt_l, F, A	; no carry when 0x00 -> 0xff
 	bc 	lcdlp1		; carry, then loop again
 	return			; carry reset so return
 
+LCD_clear:
+    movlw   0xFF
+    call    LCD_delay_ms
+    movlw   0xFF
+    call    LCD_delay_ms
+    movlw   00000001B
+    call    LCD_Send_Byte_I
+    call    LCD_delay
+    return
 
     end
 
