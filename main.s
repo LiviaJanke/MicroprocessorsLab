cbf6a0f8e0fd24de119b6dbff3c4da1f57cda17d
lme19 <lme19@skbl-418-003.ph.ic.ac.uk>
2023-11-28 11:58:34 +0000

    adding piano example code

--- main.s
+++ main.s
@@ -1,14 +1,24 @@
 #include <xc.inc>
-
+Global	Recording, Replay
+    
 extrn	UART_Setup, UART_Transmit_Message  ; external subroutines
 extrn	LCD_Setup, LCD_Write_Message, LCD_clear,keypad_output, LCD_Send_Char_D, LCD_Write_Hex 
 extrn	converter
 extrn	DAC_Setup, DAC_Int_Hi
 extrn	ADC_Setup, ADC_Read	
     
+extrn	LCD_Setup
+extrn	Init_piano
+extrn	ADC_Setup, Change_Freq
+extrn	DAC_Setup, DAC_Int_Hi, Clear_Recording, freq_replay
+    
 psect	udata_acs   ; reserve data space in access ram
 counter:    ds 1    ; reserve one byte for a counter variable
 delay_count:ds 1    ; reserve one byte for counter in the delay routine
+freq_rollover: ds 1
+delay_num:  ds 1
+Recording:  ds 1
+Replay: ds 1
     
 psect	udata_bank4 ; reserve data anywhere in RAM (here at 0x400)
 myArray:    ds 0x80 ; reserve 128 bytes for message data
@@ -25,12 +35,24 @@ psect	code, abs
 rst: 	org 0x0
  	goto	setup
 
+int_hi:	
+	org	0x0008	; high priority interrpy
+	movf	freq_rollover, W, A	; Move freq_rollover to W
+	btfsc	Replay, 0, A
+	movf	freq_replay, W, A	; Move replay to W if replay mode enabled
+	goto	DAC_Int_Hi	
+	
 	; ******* Programme FLASH read Setup Code ***********************
 setup:	bcf	CFGS	; point to Flash program memory  
 	bsf	EEPGD 	; access Flash program memory
 	call	UART_Setup	; setup UART
 	call	LCD_Setup	; setup UART
-	goto	start
+	call	Init_piano
+	call	ADC_Setup
+	call	DAC_Setup
+	setf	TRISC, A    ; all input (control unit for start & stop replay / recording and clear memory) 
+;	goto	start
+	goto	main
 	
 	; ******* Main programme ****************************************
 start: 	lfsr	0, myArray	; Load FSR0 with address in RAM	
@@ -43,37 +65,79 @@ start: 	lfsr	0, myArray	; Load FSR0 with address in RAM
 	movlw	myTable_l	; bytes to read
 	movwf 	counter, A		; our counter register
 	
-loop: 	tblrd*+			; one byte from PM to TABLAT, increment TBLPRT
-	movff	TABLAT, POSTINC0; move data from TABLAT to (FSR0), inc FSR0	
-	decfsz	counter, A		; count down to zero
-	bra	loop		; keep going until finished
-		
-	movlw	myTable_l	; output message to UART
-	lfsr	2, myArray
-	call	UART_Transmit_Message
+main:
+	btfss	Replay, 0, A
+	call	Change_Freq		; Stores detected input to W
+	movwf	freq_rollover, A	; Move W to freq-rollover
+	
+	btfsc	PORTC, 0, A      ; If Pin 0 of Port C pressed, set Recording True
+	bsf	Recording, 0, A
+	
+	btfsc	PORTC, 1, A	  ; If Pin 1 of Port C pressed, set Recording False
+	bcf	Recording, 0, A
+	
+	btfsc	PORTC, 2, A      ; If Pin 2 of Port C pressed, Clear Recording
+	call	Clear_Recording
+	
+	btfsc	PORTC, 3, A      ; If Pin 3 of Port C pressed, Set replay True
+	call	Replay_Music
+	
+	btfsc	PORTC, 4, A      ; If Pin 4 of Port C pressed, Set Replay False
+	bcf	Replay, 0, A
+	
 
-	movlw	myTable_l	; output message to LCD
-	addlw	0xff		; don't send the final carriage return to LCD
-	lfsr	2, myArray
-	call	LCD_Write_Message
-	call	LCD_clear
-	call	keypad_output
-	call	converter
-	call	LCD_Send_Char_D
-	goto	$		; goto current line in code
+	movlw	0xAA		; Add in some delay to reduce boucing issues
+	movwf	delay_num, A
+	
+loop:
+	movlw	0xFF
+	movwf	delay_count, A
+	call	delay
+	decfsz	delay_num, A
+	bra	loop
+	
+	bra	start
+	
+	goto	$		; goto current line in code	
+	
+;loop: 	tblrd*+			; one byte from PM to TABLAT, increment TBLPRT
+;	movff	TABLAT, POSTINC0; move data from TABLAT to (FSR0), inc FSR0	
+;	decfsz	counter, A		; count down to zero
+;	bra	loop		; keep going until finished
+		
+;	movlw	myTable_l	; output message to UART
+;	lfsr	2, myArray
+;	call	UART_Transmit_Message
+
+;	movlw	myTable_l	; output message to LCD
+;	addlw	0xff		; don't send the final carriage return to LCD
+;	lfsr	2, myArray
+;	call	LCD_Write_Message
+;	call	LCD_clear
+;	call	keypad_output
+;	call	converter
+;	call	LCD_Send_Char_D
+;	goto	$		; goto current line in code
 
 	; a delay subroutine if you need one, times around loop in delay_count
 	
-measure_loop:
-	call	ADC_Read
-	movf	ADRESH, W, A
-	call	LCD_Write_Hex
-	movf	ADRESL, W, A
-	call	LCD_Write_Hex
-	goto	measure_loop		; goto current line in code	
+;measure_loop:
+;	call	ADC_Read
+;	movf	ADRESH, W, A
+;	call	LCD_Write_Hex
+;	movf	ADRESL, W, A
+;	call	LCD_Write_Hex
+;	goto	measure_loop		; goto current line in code	
 
+
+	
 delay:	decfsz	delay_count, A	; decrement until zero
 	bra	delay
+	return
+	
+Replay_Music:	; Called when RC3 pressed
+	bsf	Replay, 0, A
+	lfsr	0, 0x200        ; Start memory storage at Bank 2, use FSR0
 	return
 
 	end	rst
