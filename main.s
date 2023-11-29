eb00f09332518c69bd35b0e86c6eeae05a24b490
lme19 <lme19@skbl-418-003.ph.ic.ac.uk>
2023-11-29 15:23:51 +0000

    adding piano example code

--- main.s
+++ main.s
@@ -1,34 +1,23 @@
 #include <xc.inc>
 Global	Recording, Replay
     
-extrn	UART_Setup, UART_Transmit_Message  ; external subroutines
-extrn	LCD_Setup, LCD_Write_Message, LCD_clear,keypad_output, LCD_Send_Char_D, LCD_Write_Hex 
-extrn	converter
-extrn	DAC_Setup, DAC_Int_Hi
-extrn	ADC_Read	
+;extrn	UART_Setup, UART_Transmit_Message  ; external subroutines
+extrn	LCD_Setup ;, LCD_Write_Message, LCD_clear,keypad_output, LCD_Send_Char_D, LCD_Write_Hex 
+;extrn	converter
+extrn	DAC_Setup, DAC_Int_Hi, Clear_Recording, freq_replay
+;extrn	ADC_Read	
 extrn	Init_piano
 extrn	ADC_Setup, Change_Freq
-extrn	Clear_Recording, freq_replay
+;extrn	Clear_Recording, freq_replay
     
 psect	udata_acs   ; reserve data space in access ram
-counter:    ds 1    ; reserve one byte for a counter variable
 delay_count:ds 1    ; reserve one byte for counter in the delay routine
 freq_rollover: ds 1
 delay_num:  ds 1
 Recording:  ds 1
 Replay: ds 1
     
-psect	udata_bank4 ; reserve data anywhere in RAM (here at 0x400)
-myArray:    ds 0x80 ; reserve 128 bytes for message data
 
-psect	data    
-	; ******* myTable, data in programme memory, and its length *****
-myTable:
-	db	'H','e','l','l','o',' ','W','o','r','l','d','!',0x0a
-					; message, plus carriage return
-	myTable_l   EQU	13	; length of data
-	align	2
-    
 psect	code, abs	
 rst: 	org 0x0
  	goto	setup
@@ -43,28 +32,17 @@ int_hi:
 	; ******* Programme FLASH read Setup Code ***********************
 setup:	bcf	CFGS	; point to Flash program memory  
 	bsf	EEPGD 	; access Flash program memory
-	call	UART_Setup	; setup UART
+	;call	UART_Setup	; setup UART
 	call	LCD_Setup	; setup UART
 	call	Init_piano
 	call	ADC_Setup
 	call	DAC_Setup
 	setf	TRISC, A    ; all input (control unit for start & stop replay / recording and clear memory) 
 	goto	start
-	goto	main
+	
 	
 	; ******* Main programme ****************************************
-start: 	lfsr	0, myArray	; Load FSR0 with address in RAM	
-	movlw	low highword(myTable)	; address of data in PM
-	movwf	TBLPTRU, A		; load upper bits to TBLPTRU
-	movlw	high(myTable)	; address of data in PM
-	movwf	TBLPTRH, A		; load high byte to TBLPTRH
-	movlw	low(myTable)	; address of data in PM
-	movwf	TBLPTRL, A		; load low byte to TBLPTRL
-	movlw	myTable_l	; bytes to read
-	movwf 	counter, A		; our counter register
-	goto	main
-	
-main:
+start: 
 	btfss	Replay, 0, A		; Bit Test File, Skip if Set
 	call	Change_Freq		; Stores detected input to W
 	movwf	freq_rollover, A	; Move W to freq-rollover
@@ -100,36 +78,6 @@ loop:
 	
 	goto	$		; goto current line in code	
 	
-;loop: 	tblrd*+			; one byte from PM to TABLAT, increment TBLPRT
-;	movff	TABLAT, POSTINC0; move data from TABLAT to (FSR0), inc FSR0	
-;	decfsz	counter, A		; count down to zero
-;	bra	loop		; keep going until finished
-		
-;	movlw	myTable_l	; output message to UART
-;	lfsr	2, myArray
-;	call	UART_Transmit_Message
-
-;	movlw	myTable_l	; output message to LCD
-;	addlw	0xff		; don't send the final carriage return to LCD
-;	lfsr	2, myArray
-;	call	LCD_Write_Message
-;	call	LCD_clear
-;	call	keypad_output
-;	call	converter
-;	call	LCD_Send_Char_D
-;	goto	$		; goto current line in code
-
-	; a delay subroutine if you need one, times around loop in delay_count
-	
-;measure_loop:
-;	call	ADC_Read
-;	movf	ADRESH, W, A
-;	call	LCD_Write_Hex
-;	movf	ADRESL, W, A
-;	call	LCD_Write_Hex
-;	goto	measure_loop		; goto current line in code	
-
-
 	
 delay:	decfsz	delay_count, A	; decrement until zero
 	bra	delay
