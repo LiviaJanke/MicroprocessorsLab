279e1c8dc5600a57e0afe80bda6cde31e5fce3fa
lme19 <lme19@skbl-418-003.ph.ic.ac.uk>
2023-12-06 15:17:03 +0000

    new version from Andy

--- main.s
+++ main.s
@@ -1,9 +1,13 @@
 #include <xc.inc>
 
-
+extrn	DAC_Int_Hi_sine, DAC_Int_Hi_square
+extrn	DAC_Setup_sine, DAC_Setup_square
+extrn	Load_waveform
+global	change_signal, detect_notes
+	
 psect	code, abs
 
-freq: ds 1
+;freq: ds 1
 ;port B notes
 RBC	equ 0   ;0x22 for C note
 RBCs	equ 1
@@ -14,99 +18,163 @@ RBF	equ 5
 RBFs	equ 6
 RBG	equ 7
 
-;port F notes
-RFB	equ 7
-RFAs	equ 6
-RFA	equ 5
-RFGs	equ 4
-RFG	equ 3
-RFFs	equ 2
-RFF	equ 1
-RFE1	equ 0
+;port E notes
+REB	equ 7
+REAs	equ 6
+REA	equ 5
+REGs	equ 4
+REG	equ 3
+REFs	equ 2
+REF	equ 1
+REE	equ 0
 
-;port H notes
-RHDs	equ 7
-RHD	equ 6
-RHCs	equ 5
-RHC	equ 4
-RHB	equ 3
-RHAs	equ 2
-RHA	equ 1
-RHGs	equ 0
-;RHG
+;port J notes
+RJDs	equ 7
+RJD	equ 6
+RJCs	equ 5
+RJC	equ 4
+RJB	equ 3
+RJAs	equ 2
+RJA	equ 1
+RJGs	equ 0
 
+;port C control
+RCchange	equ	0	
+RCsawtooth	equ	1
+RCsine		equ	2
+RCsquare	equ	3
+RCRecordstart	equ	4
+RCRecordstop	equ	5
+RCReplay	equ	6
+RCClear		equ	7
+	
 
-	    
-	    
-	    
+		
+delay_count:ds 1    ; reserve one byte for counter in the delay routine
+freq_rollover: ds 1
+delay_num:  ds 1
+Recording:  ds 1
+Replay: ds 1
+counter1: ds 1
+counter2: ds 1
+freq_replay: ds 1
+	  	    
 main:
 	org	0x0
-;	goto	start
-	bsf     TRISB, RBC	; Set PORTB as input
-	bsf	TRISF, RFB	; Set PORTF as input
-	bsf	TRISH, RHDs	; Set PORTH as input
-	;banksel TRISB         ; Select bank for TRISB register
-        
+	bsf	TRISB, RBG	; Set PORTB as input
+	bsf	TRISE, REE
+	bsf	TRISJ, RJDs	; Set PORTH as input
+	bsf	TRISC, RCsawtooth
+
 	org	0x100		    ; Main code starts here at address 0x100
-start:
+
+change_signal:
+	btfsc	PORTC,RCsawtooth	;check if want to change signal
+	goto	sawtooth
+	btfsc	PORTC,RCsine
+	call	DAC_Int_Hi_sine
+	;goto	sine
+	btfsc	PORTC,RCsquare
+	call	DAC_Int_Hi_square
+	goto	change_signal			;loop
+	
+	
+sawtooth:
 	movlw 	0x0
 	movwf	TRISD, A	; Port D all outputs
-	
-	;setf	TRISB, A
-	;clrf	TRISB
-;	CLRF	PORTJ	; Initialize PORTJ by
-			; clearing output latches    
-;	CLRF	LATJ	; Alternate method
-			; to clear output latches
-;	MOVLW	0CFh
-	;movlw	0x0
-	;movwf	TRISJ, A
-;	bcf	PORTJ, 1 ,0
-	
-	
 	bra 	test
+
 loop:
 	call	detect_notes
 	movff 	0x06, PORTD
 	incf 	0x06, W, A
+
 test:
 	movwf	0x06, A	    ; Test for end of loop condition
 	movlw 	0xFF	    ; The count down max
 	cpfsgt 	0x06, A
-	;call	detect_notes
-	;call	freq_change ;input the frequency number
-	call	delay	    
+	call	delay	 
+	btfsc	PORTC,RCchange	;check if want to change signal
+	goto	change_signal			;return to choose signal
 	bra 	loop		    ; Not yet finished goto start of loop again
-	;movff 	0x06, PORTJ
-;	bsf	PORTJ, 1, 0
+
 	goto 	0x0		    ; Re-run program from start
+
+sine:
+    	movlw	0xFF
+	movwf	freq_rollover, A
+	dcfsnz	counter1, A	  
+	call	Load_waveform ; Load Lookup table waveform
+	tblrd*+			; move along table
+	movff	TABLAT, LATD, A ; move value from table to port J
+	call	detect_notes
+	call	delay
+	bsf	LATH, 0, A
+	bcf	TMR0IF		; clear interrupt flag
+	btfsc	PORTC,RCchange	;check if want to change signal
+	goto	change_signal	;return to choose signal
+	goto	sine
 
 	
 detect_notes:
-	;banksel TRISB    ; Select bank for BUTTON_PIN
+	
 	call	note_check
-        ;btfsc   PORTB,ButtonC    ; Test if the button pin is clear (pressed)
-        ;goto	NoteC	    ; If pressed, jump to button_pressed
-        ;movff	0x22, freq
-	;goto    default           ; Otherwise, continue looping
+
 	return
+	
 note_check:
+	;port B note check
 	btfsc   PORTB,RBC    ; Test if the button pin is clear (pressed)
         goto	NoteRBC	    ; If pressed, jump to button_pressed
-	btfsc   PORTB,RBCs    ; Test if the button pin is clear (pressed)
-        goto	NoteRBCs	    ; If pressed, jump to button_pressed
-	btfsc   PORTB,RBD    ; Test if the button pin is clear (pressed)
-        goto	NoteRBD	    ; If pressed, jump to button_pressed
-	btfsc   PORTB,RBDs    ; Test if the button pin is clear (pressed)
-        goto	NoteRBDs	    ; If pressed, jump to button_pressed
-	btfsc   PORTB,RBE    ; Test if the button pin is clear (pressed)
-        goto	NoteRBE	    ; If pressed, jump to button_pressed
-	btfsc   PORTB,RBF    ; Test if the button pin is clear (pressed)
-        goto	NoteRBF	    ; If pressed, jump to button_pressed
-	btfsc   PORTB,RBFs    ; Test if the button pin is clear (pressed)
-        goto	NoteRBFs	    ; If pressed, jump to button_pressed
-	btfsc   PORTB,RBG    ; Test if the button pin is clear (pressed)
-        goto	NoteRBG	    ; If pressed, jump to button_pressed
+	btfsc   PORTB,RBCs    
+        goto	NoteRBCs	    
+	btfsc   PORTB,RBD    
+        goto	NoteRBD	    
+	btfsc   PORTB,RBDs    
+        goto	NoteRBDs	    
+	btfsc   PORTB,RBE    
+        goto	NoteRBE	    
+	btfsc   PORTB,RBF    
+        goto	NoteRBF	    
+	btfsc   PORTB,RBFs    
+        goto	NoteRBFs	    
+	btfsc   PORTB,RBG    
+        goto	NoteRBG	    
+	;port F note check
+	btfsc   PORTE,REE    
+        goto	NoteREE
+	btfsc   PORTE,REF    
+        goto	NoteREF
+	btfsc   PORTE,REFs    
+        goto	NoteREFs
+	btfsc   PORTE,REG    
+        goto	NoteREG
+	btfsc   PORTE,REGs    
+        goto	NoteREGs
+	btfsc   PORTE,REA    
+        goto	NoteREA
+	btfsc   PORTE,REAs    
+        goto	NoteREAs
+	btfsc   PORTE,REB   
+        goto	NoteREB
+	;port J note check
+	btfsc   PORTJ,RJDs   
+        goto	NoteRJDs
+	btfsc   PORTJ,RJD   
+        goto	NoteRJD
+	btfsc   PORTJ,RJCs    
+        goto	NoteRJCs
+	btfsc   PORTJ,RJC    
+        goto	NoteRJC
+	btfsc   PORTJ,RJB    
+        goto	NoteRJB
+	btfsc   PORTJ,RJAs    
+        goto	NoteRJAs
+	btfsc   PORTJ,RJA    
+        goto	NoteRJA
+	btfsc   PORTJ,RJGs    
+        goto	NoteRJGs
+	;if all NO, go to default
 	goto	default
 	return
 default:
@@ -114,11 +182,75 @@ default:
 	movwf	0x03
 	return
 
-freq_change:
-	movlw	freq
+
+; NOTE Branches;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;PORT J notes
+NoteRJGs:
+	movlw	0x45 
 	movwf	0x03, A
 	return
-
+NoteRJA:
+	movlw	0x40 
+	movwf	0x03, A
+	return
+NoteRJAs:
+	movlw	0x3B 
+	movwf	0x03, A
+	return
+NoteRJB:
+	movlw	0x38 
+	movwf	0x03, A
+	return
+NoteRJC:
+	movlw	0x36
+	movwf	0x03, A
+	return	
+NoteRJCs:
+	movlw	0x32 
+	movwf	0x03, A
+	return	
+NoteRJD:
+	movlw	0x2F 
+	movwf	0x03, A
+	return
+NoteRJDs:
+	movlw	0x2D 
+	movwf	0x03, A
+	return
+;PORT E notes
+NoteREE:
+	movlw	0x30 
+	movwf	0x03, A
+	return
+NoteREF:
+	movlw	0x2B 
+	movwf	0x03, A
+	return
+NoteREFs:
+	movlw	0x26 
+	movwf	0x03, A
+	return
+NoteREG:
+	movlw	0x23 
+	movwf	0x03, A
+	return
+NoteREGs:
+	movlw	0x1E
+	movwf	0x03, A
+	return	
+NoteREA:
+	movlw	0x1A 
+	movwf	0x03, A
+	return	
+NoteREAs:
+	movlw	0x16 
+	movwf	0x03, A
+	return
+NoteREB:
+	movlw	0x13 
+	movwf	0x03, A
+	return
+;PORT B notes
 NoteRBC:
 	movlw	0x20 
 	movwf	0x03, A
@@ -151,11 +283,10 @@ NoteRBG:
 	movlw	0x0C 
 	movwf	0x03, A
 	return
-	
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;	
 delay:
 	decfsz	0x03
 	bra delay
 	return
 
-	
-	end	main
+end	main
