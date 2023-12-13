c1d50718335169640987259a39b18f5780ebe903
ANDYisregistered <baidiyang2003@163.com>
2023-12-13 17:03:47 +0000

    gah

--- sawtooth.s
+++ sawtooth.s
@@ -20,8 +20,8 @@ RBD	equ 2
 RBDs	equ 3
 RBE	equ 4
 RBF	equ 5
-;RBFs	equ 6
-;RBG	equ 7
+RBFs	equ 6
+RBG	equ 7
 
 ;port E notes
 REB	equ 7
@@ -323,39 +323,39 @@ detect_notes:
 	return
 note_check:
 	;port B note check
-	btfsc   PORTB,RBC    ; Test if the button pin is clear (pressed)
-        goto	NoteRBC	    ; If pressed, jump to button_pressed
-	btfsc   PORTB,RBCs    
-        goto	NoteRBCs	    
-	btfsc   PORTB,RBD    
-        goto	NoteRBD	    
-	btfsc   PORTB,RBDs    
-        goto	NoteRBDs	    
-	btfsc   PORTB,RBE    
-        goto	NoteRBE	    
-	btfsc   PORTB,RBF    
-        goto	NoteRBF	    
-	btfsc   PORTB,RBFs    
-        goto	NoteRBFs	    
 	btfsc   PORTB,RBG    
-        goto	NoteRBG	    
+        goto	NoteRBG
+	btfsc   PORTB,RBFs    
+        goto	NoteRBFs
+	btfsc   PORTB,RBF    
+        goto	NoteRBF
+	btfsc   PORTB,RBE    
+        goto	NoteRBE
+	btfsc   PORTB,RBDs    
+        goto	NoteRBDs
+	btfsc   PORTB,RBD    
+        goto	NoteRBD
+	btfsc   PORTB,RBCs    
+        goto	NoteRBCs
+	btfsc   PORTB,RBC    ; Test if the button pin is clear (pressed)
+        goto	NoteRBC	    ; If pressed, jump to button_pressed	    
 	;port F note check
-	btfsc   PORTE,REE    
-        goto	NoteREE
-	btfsc   PORTE,REF    
-        goto	NoteREF
-	btfsc   PORTE,REFs    
-        goto	NoteREFs
-	btfsc   PORTE,REG    
-        goto	NoteREG
-	btfsc   PORTE,REGs    
-        goto	NoteREGs
-	btfsc   PORTE,REA    
-        goto	NoteREA
-	btfsc   PORTE,REAs    
-        goto	NoteREAs
 	btfsc   PORTE,REB   
         goto	NoteREB
+	btfsc   PORTE,REAs    
+        goto	NoteREAs
+	btfsc   PORTE,REA    
+        goto	NoteREA
+	btfsc   PORTE,REGs    
+        goto	NoteREGs
+	btfsc   PORTE,REG    
+        goto	NoteREG
+	btfsc   PORTE,REFs    
+        goto	NoteREFs
+	btfsc   PORTE,REF    
+        goto	NoteREF
+	btfsc   PORTE,REE    
+        goto	NoteREE
 	;port J note check
 	btfsc   PORTJ,RJDs   
         goto	NoteRJDs
@@ -385,131 +385,147 @@ default:
 	return
 ;PORT J notes
 NoteRJGs:
-	movlw	0x45 
+	movlw	0x28 
 	movwf	0x03, A
 	movlw	0x17
+	movwf	0x0E
 	return
 NoteRJA:
-	movlw	0x40 
+	movlw	0x25 
 	movwf	0x03, A
 	movlw	0x16
+	movwf	0x0E
 	return
 NoteRJAs:
-	movlw	0x3B 
+	movlw	0x22 
 	movwf	0x03, A
 	movlw	0x15
+	movwf	0x0E
 	return
 NoteRJB:
-	movlw	0x38 
+	movlw	0x1E 
 	movwf	0x03, A
 	movlw	0x14
+	movwf	0x0E
 	return
 NoteRJC:
-	movlw	0x36
+	movlw	0x1C
 	movwf	0x03, A
 	movlw	0x13
+	movwf	0x0E
 	return	
 NoteRJCs:
-	movlw	0x32 
+	movlw	0x19 
 	movwf	0x03, A
 	movlw	0x12
+	movwf	0x0E
 	return	
 NoteRJD:
-	movlw	0x2F 
+	movlw	0x17 
 	movwf	0x03, A
 	movlw	0x11
+	movwf	0x0E
 	return
 NoteRJDs:
-	movlw	0x2D 
+	movlw	0x15 
 	movwf	0x03, A
 	movlw	0x10
+	movwf	0x0E
 	return
 ;PORT E notes
 NoteREE:
-	movlw	0x30 
-	movwf	0x03, A
-	movlw	0x0F
-	return
-NoteREF:
-	movlw	0x2B 
-	movwf	0x03, A
-	movlw	0x0E
-	return
-NoteREFs:
-	movlw	0x26 
-	movwf	0x03, A
-	movlw	0x0D
-	return
-NoteREG:
-	movlw	0x23 
-	movwf	0x03, A
-	movlw	0x0C
-	return
-NoteREGs:
-	movlw	0x1E
-	movwf	0x03, A
-	movlw	0x0B
-	return	
-NoteREA:
-	movlw	0x1A 
-	movwf	0x03, A
-	movlw	0x0A
-	return	
-NoteREAs:
-	movlw	0x16 
-	movwf	0x03, A
-	movlw	0x09
-	return
-NoteREB:
 	movlw	0x13 
 	movwf	0x03, A
+	movlw	0x0F
+	movwf	0x0E
+	return
+NoteREF:
+	movlw	0x11 
+	movwf	0x03, A
+	movlw	0x0E
+	movwf	0x0E
+	return
+NoteREFs:
+	movlw	0x0F 
+	movwf	0x03, A
+	movlw	0x0D
+	movwf	0x0E
+	return
+NoteREG:
+	movlw	0x0E 
+	movwf	0x03, A
+	movlw	0x0C
+	movwf	0x0E
+	return
+NoteREGs:
+	movlw	0x0D
+	movwf	0x03, A
+	movlw	0x0B
+	movwf	0x0E
+	return	
+NoteREA:
+	movlw	0x0B 
+	movwf	0x03, A
+	movlw	0x0A
+	movwf	0x0E
+	return	
+NoteREAs:
+	movlw	0x0A 
+	movwf	0x03, A
+	movlw	0x09
+	movwf	0x0E
+	return
+NoteREB:
+	movlw	0x09 
+	movwf	0x03, A
 	movlw	0x08
+	movwf	0x0E
 	return
 ;PORT B notes
 NoteRBC:
-	movlw	0x20 
+	movlw	0x08 
 	movwf	0x03, A
 	movlw	0x07
 	movwf	0x0E
 	return
 NoteRBCs:
-	movlw	0x1D 
+	movlw	0x07 
 	movwf	0x03, A
 	movlw	0x06
 	movwf	0x0E
 	return
 NoteRBD:
-	movlw	0x1A 
+	movlw	0x07 
 	movwf	0x03, A
 	movlw	0x05
 	movwf	0x0E
 	return
 NoteRBDs:
-	movlw	0x17 
+	movlw	0x06 
 	movwf	0x03, A
 	movlw	0x04
 	movwf	0x0E
 	return
 NoteRBE:
-	movlw	0x14 
+	movlw	0x06
 	movwf	0x03, A
 	movlw	0x03
 	movwf	0x0E
 	return	
 NoteRBF:
-	movlw	0x11 
+	movlw	0x05 
 	movwf	0x03, A
 	movlw	0x02
 	movwf	0x0E
 	return	
 NoteRBFs:
-	movlw	0x0E 
+	movlw	0x05
 	movwf	0x03, A
 	movlw	0x01
 	movwf	0x0E
 	return
 NoteRBG:
-	movlw	0x11 
+	movlw	0x05 
 	movwf	0x03, A
 	movlw	0x19
 	movwf	0x0E
